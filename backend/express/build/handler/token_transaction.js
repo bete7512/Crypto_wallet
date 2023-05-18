bacfcae335ffbebf832721f2ddd78ea942fd18dc
user <betekbebe@gmail.com>
2023-05-18 21:58:49 +0300

    do something

--- backend/express/build/handler/token_transaction.js
+++ backend/express/build/handler/token_transaction.js
@@ -14,6 +14,7 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 var Tegera_Address = '0x09871Adb124aab581046EA38c26C6af2e88F280E';
 var Tether_Address = '0x222fB5507acD3Da78351Be60271fa9537b07Cdc3';
+//0x222fB5507acD3Da78351Be60271fa9537b07Cdc3
 dotenv.config();
 var config = {
   apiKey: '5NOA_cRPiKibJ6QRvlAQamuQWFKWkjJJ',
--- backend/express/build/web3/web3.js
+++ backend/express/build/web3/web3.js
@@ -15,6 +15,7 @@ function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && 
 
 // import Contract from 'web3-eth-contract'
 // import Personal from 'web3-eth-personal'
+
 dotenv.config();
 var web3 = new _web["default"]('wss://sepolia.infura.io/ws/v3/7adda8f39ad04bf2b5709fe8a04fbbdd');
 // process.env.INFURA_SEPOLIA_END_POINT)
