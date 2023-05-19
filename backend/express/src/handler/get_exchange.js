0527dcd0bc359861b6813892722b3bffe7b51620
user <betekbebe@gmail.com>
2023-05-19 16:37:39 +0300

    Exhange for crypto currency

--- backend/express/src/handler/get_exchange.js
+++ backend/express/src/handler/get_exchange.js
@@ -31,12 +31,38 @@ const handler = async (req, res) => {
         method: 'GET',    
     })
     const data2 = await response2.json()    
-    console.log(data2 );
+    let data3 = await fetch('https://min-api.cryptocompare.com/data/top/exchanges/full?fsym=USDT&tsym=USD',
+    {
+        headers: {     
+            'X-CMC_PRO_API_KEY': '4a18b767-4cce-45b7-a618-f277624d705e',         
+        },
+        method: 'GET',     
+    })
+    data3 = await data3.json()
 
-    console.log(data); 
-
-    let exchange = {...data, ...data2}
-    return res.status(200).json(exchange)
+    let data4 = await fetch('https://min-api.cryptocompare.com/data/top/exchanges/full?fsym=XLM&tsym=USD',
+    {
+        headers: {  
+            'X-CMC_PRO_API_KEY': '4a18b767-4cce-45b7-a618-f277624d705e',        
+        },
+        method: 'GET',   
+    })
+    data4 = await data4.json()
+    let data5 = await fetch('https://min-api.cryptocompare.com/data/top/exchanges/full?fsym=ADA&tsym=USD',
+    {
+        headers: {  
+            'X-CMC_PRO_API_KEY': '4a18b767-4cce-45b7-a618-f277624d705e',        
+        },
+        method: 'GET',    
+    })
+    data5 = await data5.json() 
+    let ETH_exchanges = data.Data.AggregatedData 
+    let BTC_exchanges = data2.Data.AggregatedData    
+    let USDT_exchanges = data3.Data.AggregatedData   
+    let XLM_exchanges = data4.Data.AggregatedData   
+    let ADA_exchanges = data5.Data.AggregatedData      
+    let exchanges = [{eth:ETH_exchanges}, {btc:BTC_exchanges}, {tether:USDT_exchanges}, {xlm:XLM_exchanges}, {cardano:ADA_exchanges}    ] 
+    return res.status(200).json({ exchanges }) 
   } catch (error) {
     console.log(error)
     return res.status(500).json({ error: error.message })
