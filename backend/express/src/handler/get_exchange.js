c86aa0df2aff0521e42fa1238c2bac83699ad383
user <betekbebe@gmail.com>
2023-05-19 15:53:46 +0300

    do

--- backend/express/src/handler/get_exchange.js
+++ backend/express/src/handler/get_exchange.js
@@ -3,15 +3,23 @@ import { web3 } from '../web3/web3.js'
 const handler = async (req, res) => {
   try {
     let response = null
-    response = await fetch(
-      'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-      {
-        headers: {
-          'X-CMC_PRO_API_KEY': '4a18b767-4cce-45b7-a618-f277624d705e',
+    // response = await fetch(
+    //   'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+    //   {
+    //     headers: {
+    //       'X-CMC_PRO_API_KEY': '4a18b767-4cce-45b7-a618-f277624d705e',
+    //     },
+    //     method: 'GET',
+    //   },
+    // )
+
+    response = await fetch('https://sandbox-api.coinmarketcap.com/v1/exchange/assets',
+    {
+        headers: {  
+            'X-CMC_PRO_API_KEY': '4a18b767-4cce-45b7-a618-f277624d705e',   
         },
-        method: 'GET',
-      },
-    )
+        method: 'GET',  
+    })
     const data = await response.json()  
 
     return res.status(200).json({ exchange: data })
